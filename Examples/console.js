54f8c971ee94e0b0eaf7b30cc904e9cc00f5c2e2
Mohammad Ebrahimi <moh.ebrahimi2000@gmail.com>
2023-01-13 11:27:34 +0330

    Add get_bad_word functionality

--- Examples/console.js
+++ Examples/console.js
@@ -1,7 +1,11 @@
-const { detect } = require('../src/index'); //you should write "persian-bad-words"
+const { detect, get_bad_word } = require('../src/index'); //you should write "persian-bad-words"
 
 console.log(detect("کی ر")); //returns true
 console.log(detect("کی‌ر")); //returns true
 console.log(detect("سلام زنتو من گای یدم")); //return true
 console.log(detect("سلام سوتون چطوری امیدوارم عشق کنی بامون")); //returns true
-  
+
+console.log(get_bad_word("کی ر")); //returns true
+console.log(get_bad_word("کی‌ر")); //returns true
+console.log(get_bad_word("سلام زنتو من گای یدم")); //return true
+console.log(get_bad_word("سلام سوتون چطوری امیدوارم عشق کنی بامون")); //returns true
